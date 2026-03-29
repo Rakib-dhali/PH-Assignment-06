370a76083cac898c48111aeff156b10b86ebe539
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-03-29 21:25:43 +0600

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+     <h2 className="bg-white text-purple-500 text2xl text-center font-bold py-20">Programming hero assignment 6</h2>
+    </>
+  )
+}
+
+export default App
